9c3c8e76610fbf42854214a28221a0acb190fd77
Samir Elias <samirelias@gmail.com>
2018-10-08 21:07:36 -0700

    moved code to Search.js

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,7 +1,7 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 
-class BooksPage extends React.Component {
+class BooksPage extends Component {
     render() {
 
         return (
