7a35a3e22718565183669021c4f20b3683371243
Samir Elias <samirelias@gmail.com>
2018-10-10 10:51:33 -0700

    cleaned Bookshelf and added the <select> function

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 
 class Book extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      shelf: this.props.shelf
+    }
+  }
+ 
+  handleChange(el) {
+    this.setState({ shelf: el.value });
+  }
+
+
   render() {
+  
     return (
       <li>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.props.cover}}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select 
+                defaultValue={this.state.shelf} 
+                onChange={e => this.handleChange(e)}
+              >
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
