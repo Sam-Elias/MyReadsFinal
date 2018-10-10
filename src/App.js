03b0497f2c3248a96de5812c8986ef085c5b2825
Samir Elias <samirelias@gmail.com>
2018-10-09 22:39:36 -0700

    added BooksAPI method to get books

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react'
-
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BooksPage from './pages/Books'
 import SearchPage from './pages/Search';
 
 class BooksApp extends Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      books: [],
+      showSearchPage: false
+    }
   }
+
+  componentDidMount = () => {
+    BooksAPI.getAll()
+      .then(books => this.setState({ books: books }))
+      .catch(err => { });
+  }
+
 
   render() {
     return (
       <div className="app">
+      {console.log(this.state.books)}
         {this.state.showSearchPage ? (
-          <SearchPage />
+          
+          <SearchPage  />
         ) : (
           <BooksPage />
           
